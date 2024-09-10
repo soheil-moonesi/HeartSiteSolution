df473083a768f9b3b462117246c6b0c21d5d0a6f
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-09-10 21:18:27 +0330

    Add : UserGet Test Function

---
+++ HeartSite/Controllers/UserGetController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeartSite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserGetController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult UserGet()
+        {
+            return Ok(UserManager.users[0].Name);
+        }
+    }
+}
